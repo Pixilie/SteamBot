4255fd9a034d436b1a84eeb67f3d3ee9eaedf2c0
Kristen <kristen.couty@gmail.com>
2022-05-17 22:36:42 +0200

    Get playtime function
    GetTime function almost finish

--- index.js
+++ index.js
@@ -3,8 +3,29 @@ import { Client, Intents } from 'discord.js';
 import config from './config.json' assert { type: "json" };
 import { REST } from '@discordjs/rest';
 import { Routes } from 'discord-api-types/v9';
-import { SlashCommandBuilder  } from '@discordjs/builders';
-  
+import { SlashCommandBuilder } from '@discordjs/builders';
+import fetch from 'node-fetch';
+
+//Variables
+let url = "http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=9D2CF1FA472E23059BB3D8657A5C85D3&steamid=76561198807619278&format=json"
+let Time = 0
+
+// Get time
+function GetTime(url) {
+	let request = fetch(url)
+	let response = request.json
+	let games = response.get('response') // .get non reconnu
+	let timePlayed = games.get('games') // .get non reconnu
+
+	//TODO .get bug // Other way than a for to get "playtime_forever"
+	for (let i in timePlayed) {
+		let Time = (i["playtime_forever"]) + Time
+	}
+
+	return Time
+}
+GetTime(url)
+
 // New client
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
